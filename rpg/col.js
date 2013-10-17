1512bfaaf6eeb78f562070804e9ad373a60efff1
Abdul Alzaid <aalzaid1@lion.lmu.edu>
2013-10-17 07:40:51 -0700

    Final edit.
    editing JS file, by removing the class example!

--- rpg/col.js
+++ rpg/col.js
@@ -1,13 +1,3 @@
-// Big things have small beginnings...
-$(function () {
-
-    $("#confirm-delete-button").click(function () {
-        console.log("Delete confirmed!!!!!");
-
-        // Now we dismiss the dialog.
-        $('#deleteModal').modal('hide');
-    });
-});
 
 $(function() {
 	$("#help-button").popover();
