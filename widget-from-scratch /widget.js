71483e23643087f4ec1a7620f359aed4d81f83c5
Abdul Alzaid <aalzaid1@lion.lmu.edu>
2013-12-02 23:26:40 -0800

    Adding a checkbox for entering text when "enter is hit." Also, adding jQuery functionality

--- widget-from-scratch /widget.js
+++ widget-from-scratch /widget.js
@@ -1,8 +1,21 @@
 $(function() {
-
-  $("#submit").click(function(){
+  //Send on "enter" click.
+  $("#textbox").keypress(function(event) {
+    if(event.which == 13) { //when enter is hit.
+         if($("#enter").prop("checked")) { //see if box is checked.
+             var textValue = $("#textbox").val();
+             $("#container").html(textValue);
+             $("#textbox").val(" ");//clear out the text field.
+             event.preventDefault(); // to prevent jumping a space when hitting enter.
+         }
+      }
+    });
+  
+  //send when "send button" is clicked.
+  $("#send").click(function(){
         var textValue = $("#textbox").val();
-        $("#display").html(textValue);
+        $("#container").html(textValue);
         $("#textbox").val(" ");
     });
+  
   });
