c31e78d753c996628a19fa9a057253372d522b0c
dondi <dondi@lmu.edu>
2013-12-08 20:28:48 -0800

    Inline comments and notes for assignment 1126.

--- widget-from-scratch /widget.js
+++ widget-from-scratch /widget.js
@@ -1,3 +1,5 @@
+// JD: These declarations do not follow the style that we have adopted
+//     in class for declaring functions.  You should adjust this accordingly.
 function send_message(message) {
     $("#container").html("<span class = 'bot'>ChatBox: </span>" + message);
 }
@@ -7,12 +9,25 @@ function username() {
 }
 
 $(function() {
-  
+    // JD: As I've mentioned to you in an email, this code is not yet in the
+    //     form of a plugin, and it should be.  The process of doing this involves
+    //     putting some thought into what your plugin's API should be, then building
+    //     your code around that API.  What is the intended scope of the plugin?
+    //     The entire chatbox?  Or just the drag-and-drop text field?
+    //
+    //     These are the kinds of questions you'll need to answer before wrapping
+    //     your code up within a jQuery plugin.
 
   username();
   //Send on "enter" click.
-  $("#textbox").keypress(function(event) {
+    // JD: Note how your code is bound to IDs (# selectors), thus severely using
+    //     the reusability of your work.  Consider switching to repeatable values
+    //     like classes.
+  $("#textbox").keypress(function(event) { // JD: Space after "function" please.
     if(event.which == 13) { //when enter is hit.
+        // JD: Your indentation scheme is showing inconsistency here.  You started
+        //     out indenting two spaces at a time, but now you went for 5 spaces.
+        //     Choose an indent width and stick with it.
          if($("#enter").prop("checked")) { //see if box is checked.
              var userName = "<span class = 'username' = >You: </span>";
              var textValue = $("#textbox").val();
@@ -27,33 +42,59 @@ $(function() {
     });
 
   //send when "send button" is clicked.
+    // JD: Please compare the function below to the function above.  I think you
+    //     will agree with me that when "Send when Enter is clicked" is checked,
+    //     then hitting Enter and clicking the Send button should behave in *exactly*
+    //     the same way.  So what does that mean?  Well, when exactly the same thing
+    //     is supposed to happen, then you should call *exactly the same code*.
+    //
+    //     This is not what is happening here: instead, you have a near *copy* of the
+    //     code that you have above.  This is very dangerous; what if you want to
+    //     revise what happens upon Send?  Now you need to modify two copies of code.
+    //     Sometimes you might forget, then all of a sudden hitting Enter will not
+    //     do the same thing as clicking Send.  Be careful; that's precisely why we
+    //     seek proper separation of concerns.
   $("#send").click(function(){
         var userName = "<span class = 'username' = >You: </span>";
-        username();
+        username(); // JD: I rest my case.
         var textValue = $("#textbox").val();
         var currentText = $("#container").html();
         $("#container").html(currentText + "<br>" + userName +textValue);
         $("#textbox").val(" ");
         $("#container").scrollTop($("#container").prop("scrollHeight"));
-        });
+        }); // JD: Bad indent here.
   
   
   //the drag and drop function!!!!
-  $(document).ready(function() {
+  $(document).ready(function() { // JD: No need to call document.ready again;
+                                 //     the $() call at the top already does it!
         var stack = $("#container").html();
+    // JD: On the one hand, it is good that you found jQuery UI to implement
+    //     drag-and-drop functionality.  On the other hand, this makes you
+    //     miss out on an opportunity to implement this yourself.
+    //
+    //     Still, I think you have also discovered that even with these helpers,
+    //     things don't just work like magic.  As I pointed out in the email I
+    //     sent you on December 3, there is a crucial bug in your drop handler
+    //     that wipes out the droppable functionality in the text box.
         $(".boxed").draggable({helper: "clone"});
         $("#textbox").droppable({
             accept:".boxed",
-            drop: function(ev, ui) {
+            drop: function(ev, ui) { // JD: Space after "function" again.
+                // JD: This should be indented by an additional level (see where
+                //     this comment starts).
             var droppedItem = $(ui.draggable).clone();
             var currentDraggedValue = droppedItem.text();
             $(this).html(currentDraggedValue);
             
             }
-            });
+            }); // JD: Meanwhile, these three })'s are indented too far.
         });
 
   });
-
-
-
+// JD: Remember, indentation is not just about being "pretty" (although the code
+//     does look visually better).  The purpose of indentation is to *properly
+//     represent the structure of your code*.  If you do not indent properly, then
+//     (a) you make the structure of your code harder to discern, and (b) you make
+//     the reader doubt your own understanding of the structure of teh code.  You
+//     don't want either of these to happen.
